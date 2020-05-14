959f229e77f5a4fca58c1c67107c2b8c5ad81ab2
Dmitry Borutya <borutia@mail.ru>
2020-05-14 23:31:07 +0500

    save

--- www_data/data/js/log_in.js
+++ www_data/data/js/log_in.js
@@ -126,11 +126,14 @@ function click_log_in()
 
                 $.cookie('id', data.user_id);
                 //$.cookie('TeamAuth', data.token);
+                
                 $.cookie("cookie_name", "cookie_value", { 
                     expires: 3, 
-                    path: "/" ,
-                    domain: "your_site.ru" 
+                    path: "/", 
+                    domain: "your_site.ru", 
+                    secure: true 
                 });
+                
             
                 location.href = './www_data/data/view/personal_area/';
                 /*
