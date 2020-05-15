35acc41ee84788c8f119a005190abf358a5106b2
Dmitry Borutya <borutia@mail.ru>
2020-05-15 18:51:47 +0500

    save

--- www_data/data/js/projects.js
+++ www_data/data/js/projects.js
@@ -3,4 +3,13 @@ $(function (){
         var project_id = $(this).attr('id');
         location.href = '../project/';// + project_id + '/';
     });
+    set_scroll_height('.form_project');
 });
+
+//установить высоту блока для скролла
+function set_scroll_height(block) {
+    //получаем высоту страницы
+    var h_all = $(document).outerHeight(true);
+    //устанавливаем высоту блока для прокрутки
+    $(block).css('height', h_all - 93);
+}
