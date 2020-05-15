5797c18f41c9ccd3ea6893cf7a69ec08ec2fddc5
Dmitry Borutya <borutia@mail.ru>
2020-05-15 10:51:47 +0500

    save

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php
 header('Access-Control-Allow-Origin: *');
-setcookie('name', 'jeff', $y2k, '~/myhome', '.domain.com');
+setcookie('name', 'jeff');
+//setcookie('name', 'jeff', $y2k, '~/myhome', '.domain.com');
 include_once("www_data/data/index.html")
 
 ?>
